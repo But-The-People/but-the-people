21bc67e0ecb8c61e67858c45d75c769892c3f3b2
Voker57 <voker57@gmail.com>
2025-06-24 03:44:04 +0300

    redistribution of yields

--- doc/production-tree.rb
+++ doc/production-tree.rb
@@ -2,76 +2,87 @@
 # this scripts generates the graph in graphviz format
 
 tiles = {
+	# hill bonuses: 3 ore,3 coal, -1 raw salt, -1 organics
+	# rivers add +1 to most organics and -1 to ore/coal
+
+
+	# forests
+
 	"coniferous forest" => "3 lumber,2 fur",
 	"decidious forest" => "2 lumber,2 fur",
+	# not so good for wildlife
 	"dense forest" => "4 lumber,1 fur",
-	"jungle" => "4 lumber,2 exotic wood,3 coffee berries,2 premium fur",
+	# more on exotic side
+	"jungle" => "2 lumber,2 exotic wood,3 coffee berries,2 premium fur",
+	# good for wildlife
 	"light forest" => "1 lumber,3 fur",
+	# even more exotic
 	"mangroves" => "1 lumber,3 exotic wood",
+	# woodcutter's paradise
 	"northern forest" => "3 lumber,3 exotic wood,3 fur,3 premium fur",
 	"tropical grove" => "3 lumber,3 exotic wood",
 	"thicket" => "3 fur,1 lumber",
 	"ancient northern forest" => "4 lumber,3 exotic wood,3 fur,3 premium fur",
 
-	# plains
-	"desert" => "3 rock salt",
+	# kinda overloaded but it's a lush biome. No cattle to avoid yield overload.
+	"grassland" => "2 food,3 hemp,3 horses,3 sheep,3 tobacco",
+	"grassland hills" => "2 ore,2 coal,1 food,3 grapes,2 sheep,2 horses",
 
-	"grassland" => "2 food,3 hemp,2 horses,3 cattle,1 chicken,3 tobacco",
+	# a bit less lush than grassland but more friendly to crops
+	"plains" => "3 food,2 horses,2 cattle,3 barley",
+	"plains hills" => "3 olives,2 food,1 cattle,1 horses,3 ore,3 coal",
+
+	# lots of exposed  rock
+	"rock steppes" => "3 stone,1 ore,1 coal",
+	"rock steppes hills" => "3 stone,4 ore,4 coal",
+
+	# chicken can be here ok, clay is available, some food
+	"wetland" => "2 chicken,1 clay,2 food",
+	"wetland hills" => "1 food,1 chicken,2 clay",
+
+	# cattle heaven, no sheep because well you know. Best for bushes.
+	"prairie" => "2 food,2 horses,3 cattle,3 cotton,3 indigo",
+	"prairie hills" => "3 red pepper,3 ore,3 coal",
+
+	# salt pans, small exotic animals
+	"desert" => "3 rock salt,1 premium fur",
+	"desert hills" => "3 ore,2 rock salt,3 coal",
+
+	# not much going on here but one can farm and even ranch a bit
+	"shrubland" => "1 food,1 cattle,1 chicken",
+	"shrubland hills" => "3 ore,3 coal",
+
+	# ok for animals, unique crops
+	"savannah" => "2 food,2 cattle,2 sheep,3 coffee berries,3 sugar",
+	# a bit too overgrown for mining
+	"savannah hills" => "3 coca,1 sheep,2 coal,2 ore,1 food",
+
+	# should be some food like rice, and chicken don't need much space
+	"marsh" => "2 chicken,2 food,2 clay",
+	"marsh hills" => "1 food,1 chicken,3 clay",
+
+	# sheep can tough it out, and chicken are small
+	"taiga" => "1 food,1 chicken,1 sheep",
+	"taiga hills" => "2 ore,2 coal,3 maple syrup",
+
+	# furry animals around, otherwise not much
+	"tundra" => "1 fur,1 premium fur",
+	"tundra hills" => "2 ore,1 fur,1 premium fur,2 coal",
+
+	# furry, hardy animals and rugged terrain good for mining
+	"permafrost" => "1 ore,1 coal,1 premium fur",
+	"permafrost hills" => "4 ore,1 premium fur,4 coal",
 
 	"ice lake" => "1 food",
 
 	"lake" => "2 food",
 	"large river" => "2 food",
-	"marsh" => "3 chicken,3 food",
-	"permafrost" => "1 ore,1 fur",
 
-	"plains" => "3 food,1 horses,1 cattle,3 barley",
-	"prairie" => "2 food,3 horses,2 cattle,3 cotton,3 indigo",
-
-	"rock steppes" => "3 stone,1 ore",
-
-	"savanna" => "2 food,2 cattle,1 chicken,3 coffee berries,3 sugar",
-
-	"shrubland" => "1 food,2 chicken",
-
-	"taiga" => "1 food",
-
-	"tundra" => "1 fur,1 premium fur",
-
-	"wetland" => "1 chicken,1 clay,2 food",
-
-	# hills
-
-	"desert hills" => "3 ore,2 rock salt,3 ore",
-
-	"grassland hills" => "2 ore,2 coal,1 food,3 grapes,3 sheep",
-
-	"marsh hills" => "2 food,2 chicken,3 clay",
-
-	"permafrost hills" => "1 ore,1 fur,1 coal",
-
-	"tundra hills" => "2 ore,2 fur,2 coal",
-
-	"taiga hills" => "2 ore,2 coal,1 sheep,3 maple syrup",
-
-	"plains hills" => "3 olives,1 food,2 sheep,3 ore,3 coal",
-
-	"prairie hills" => "3 red pepper,2 sheep,3 ore,3 coal",
-
-	"rock steppes hills" => "3 stone,3 ore,2 coal",
-
-	"savanna hills" => "3 coca,2 sheep,2 coal,2 ore,1 food",
-
-	"shrubland hills" => "1 food,1 sheep,3 ore,3 coal",
-
-	"wetland hills" => "1 food,2 chicken,2 clay",
-
+	# this mostly overwrites stuff
 	"peak" => "3 ore,3 coal,3 stone",
 	"whales" => "3 whale fat",
 	"ocean" => "2 food",
-	}
-
-	# tier 1
+}
 
 $prodchains = []
 def chain(*args)
