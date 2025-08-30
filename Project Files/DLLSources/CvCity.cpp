94f5bd7b179f9c58429e49e75f67731f1647d4c9
Nightinggale <Nightinggale.coding@gmail.com>
2025-08-30 12:34:51 +0200

    Code cleanup: removed more DllExports, mainly related to receiving network messages

--- Project Files/DLLSources/CvCity.cpp
+++ Project Files/DLLSources/CvCity.cpp
@@ -1037,9 +1037,9 @@ void CvCity::chooseProduction(UnitTypes eTrainUnit, BuildingTypes eConstructBuil
 }
 
 
-int CvCity::getCityPlotIndex(const CvPlot* pPlot) const
+CityPlotTypes CvCity::getCityPlotIndex(const CvPlot* pPlot) const
 {
-	return plotCityXY(this, pPlot);
+	return (CityPlotTypes)plotCityXY(this, pPlot);
 }
 
 
